f30484e3d4d06134b0f4b2e08c7ee9c188dd3a34
emon <tj.emon33@gmail.com>
2025-01-12 10:26:06 +0600

    dynamic submit

--- src/App.js
+++ src/App.js
@@ -89,8 +89,29 @@ function Frined({ friend }) {
 function FormAddFriend() {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setImage("https://i.pravatar.cc/48");
+    setName("");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🤼‍♂️ Name</label>
       <input
         value={name}
